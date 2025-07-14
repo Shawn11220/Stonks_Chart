d630e4bbbcd3b00c05c7ab00e2d8a708b428eb96
Shawn <shawnfrost11220@gmail.com>
2025-07-14 16:51:09 +0000

    idk

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,7 @@
 #!bin/bash
 echo "Starting the application..."
 
-cd /home/ubuntu/prodapp || exit 1
-source venv/bin/activate
+source /home/ubuntu/prodapp/venv/bin/activate
 
 nohup uvicorn main:app --host 0.0.0.0 --port 8000 > app.log 2>&1 &
 echo "Application started. Logs are being written to app.log."
